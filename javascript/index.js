eb26f3abf55c6fe9c5e3d2db6e3f3f2c00d33da0
Nate Garrow <garrowphotography@gmail.com>
2018-08-29 15:09:16 -0500

    working zip code checker!

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,38 @@
-function zipChecker(zip)  {
-  if(zip > 20000)  {
-    return true;
-  } else {
-    return false;
+//ideas and inspiration came from foodar script.js (ownership of Revature Pro)
+//universal variables
+
+var cityElem = "zipResult";
+var cityButton = "cityChecker";
+var cityInput = "userZip";
+
+function findDriver(){
+  //get zip and check if in valid area
+  var btn = g("cityChecker");
+	if (btn != null){
+
+    btn.addEventListener("click", function zipChecker() {
+
+      console.log("working...");
+      var userZip = g(cityInput).value;
+      var resultElem = g(cityElem);
+
+      if(userZip != null && userZip == '76028')  {
+          resultElem.innerHTML = "We cover that area!";
+          resultElem.style.color = "green";
+        } else {
+          resultElem.innerHTML = "Unfortunately, you are not in a delivery zone";
+          resultElem.style.color = "red";
+        }
+    });
   }
 }
 
-function findDriver(userZip) {
-  var validArea = zipChecker(userZip);
 
-  while(!validArea) {
-    if(userZip = 00000) {break;}
-    zipChecker(userZip);
-  }
-  if(validArea){
-    console.log("Unfortunately, you are outside the Foodar delivery area.");
-  } else {
-    console.log("Zip code search: aborted");
-  }
+//shorthand
+function g(id){
+	return document.getElementById(id);
 }
+
+
+findDriver();
+//updateCity();
