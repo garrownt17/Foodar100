022c3a9a50c01b3577824f424f4af681199c6815
Nate Garrow <garrowphotography@gmail.com>
2018-09-04 10:58:01 -0500

    added optional zip codes for testing

---
+++ Foodar-wp/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <div id="footer">
+    <nav class="footer-nav">
+      <p>Site Map</p>
+      <ul>
+        <a href="./index.html"><li>Home</li></a>
+        <a href="./about.html"><li>About Foodar</li></a>
+        <a href="./request.html"><li>Request Delivery</li></a>
+        <a href="./register.html"><li>Register Now</li></a>
+      </ul>
+    </nav>
+    <div class="footer-legal">
+      <ul>
+        <li>&copy; 2018 Foodar, Inc.</li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms &amp; Conditions</a></li>
+      </ul>
+    </div>
+    <h3>Have a great day!</h3>
+  </div>
+  <div id="footAdvert">
+    <p>This site belongs to the web porfolio of Nathan Garrow and is based of the RevaturePro project, Foodar 100. See "About Foodar" for more information. </p>
+  </div>
+</footer>
---
+++ Foodar-wp/header.php
@@ -0,0 +1,16 @@
+<div id="preHeader">
+  <div id="headAdvert">
+    <p>Today's Special Offer: <span id="dailyOffer">Save up to 25% on select Sit-down meals!</span></p>
+    <button id="closeHeader" onclick="closeAdvert()">Close Daily Offer</button>
+  </div>
+</div>
+<header>
+    <img src="./images/logo.png" alt="Foodar Logo"/>
+    <nav>
+      <ul>
+        <a href="./about.html"><li>About Foodar</li></a>
+        <a href="./request.html"><li>Request Delivery</li></a>
+        <a href="./register.html"><li>Register Now</li></a>
+      </ul>
+    </nav>
+</header>
